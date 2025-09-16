db80e673833de0d6d9946a0e65f6f8b9a6d38169
Fernando Giacomino <fernando.giacomino@dersrl.ar>
2025-09-16 13:20:53 -0300

    Define

--- website_sale_product_brand/static/src/js/brand_snippet.js
+++ website_sale_product_brand/static/src/js/brand_snippet.js
@@ -1,33 +1,26 @@
 /** @odoo-module **/
 import publicWidget from '@web/legacy/js/public/public_widget';
 
-odoo.define('website_sale_product_brand.brand_snippet', function (require) {
-    const publicWidget = require('web.public.widget');
-
-    publicWidget.registry.BrandSnippet = publicWidget.Widget.extend({
-        selector: '.s_brand_grid',
-        start: function () {
-            this._applyGridLayout();
-            this._toggleSearchForm();
-        },
-
-        _applyGridLayout: function () {
-            const columns = parseInt(this.$el.data('brand-columns')) || 4;
-            const columnClass = `col-md-${Math.floor(12 / columns)}`;
-            this.$el.find('.brand-grid .brand_item').each(function () {
-                // Remove previous col-* classes if any
-                $(this).removeClass(function (index, className) {
-                    return (className.match(/(^|\s)col-\S+/g) || []).join(' ');
-                });
-                $(this).addClass(columnClass);
+publicWidget.registry.BrandSnippet = publicWidget.Widget.extend({
+    selector: '.s_brand_grid',
+    start() {
+        this._applyGridLayout();
+        this._toggleSearchForm();
+    },
+    _applyGridLayout() {
+        const columns = parseInt(this.$el.data('brand-columns')) || 4;
+        const columnClass = `col-md-${Math.floor(12 / columns)}`;
+        this.$el.find('.brand-grid .brand_item').each(function () {
+            $(this).removeClass(function (_, cls) {
+                return (cls.match(/(^|\s)col-\S+/g) || []).join(' ');
             });
-        },
-
-        _toggleSearchForm: function () {
-            const showSearch = this.$el.data('brand-search');
-            if (showSearch === false || showSearch === 'false') {
-                this.$el.find('.brand-search-form').hide();
-            }
+            $(this).addClass(columnClass);
+        });
+    },
+    _toggleSearchForm() {
+        const showSearch = this.$el.data('brand-search');
+        if (showSearch === false || showSearch === 'false') {
+            this.$el.find('.brand-search-form').hide();
         }
-    });
+    }
 });
